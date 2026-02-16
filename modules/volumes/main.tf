96bdac66aa5dedc935f028fdfa891690d0f2a529
Francisco <franciscojgc98@gmail.com>
2026-02-16 09:10:55 +0100

    .

--- modules/volumes/main.tf
+++ modules/volumes/main.tf
@@ -8,23 +8,23 @@ terraform {
 
 resource "docker_volume" "minio" {
   name = "minio-data"
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 }
 
 resource "docker_volume" "postgres" {
   name = "postgres-data"
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 }
 
 resource "docker_volume" "airflow_logs" {
   name = "airflow-logs"
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 }
 
 resource "docker_volume" "airflow_dags" {
